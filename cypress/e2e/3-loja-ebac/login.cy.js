62314682fd8ba273d2bedaf7452e57b74fb3b0c3
Sousa Roque Ferreira <gsousar@minsait.com>
2025-08-28 23:14:17 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,15 @@
+///<reference types= "cypress"/>
+describe('Funcionalidade: Login', () => {
+    it ('Deve fazer login com sucesso', () => {
+         cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+         cy.get('#username').type('gisleine.teste@teste.com.br')
+         cy.get('#password').type('243568')
+         cy.get('.woocommerce-form > .button').click()
+         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, gisleine.teste (não é gisleine.teste? Sair)')
+
+        
+         
+    })
+   
+    
+})
